b4c3402f9dba79f0dad2385ea8cb49510ac13896
Manolo Edge <nombrekeff@rallf.com>
2022-07-19 15:36:20 +0200

    lint: fixed lint errors

--- db/fixtures/001_custom_webhook.rb
+++ db/fixtures/001_custom_webhook.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 WebHookEventType.seed do |b|
-    b.id = 101
-    b.name = "remove_like"
+  b.id = 101
+  b.name = "remove_like"
 end
--- plugin.rb
+++ plugin.rb
@@ -9,45 +9,44 @@
 enabled_site_setting :discourse_remove_like_webhook_enabled
 
 after_initialize do
-    SeedFu.fixture_paths << Rails.root.join("plugins", "discourse_dislike_webhook", "db", "fixtures").to_s
-    register_seedfu_fixtures(Rails.root.join("plugins", "discourse_dislike_webhook", "db", "fixtures").to_s)
+  SeedFu.fixture_paths << Rails.root.join("plugins", "discourse_dislike_webhook", "db", "fixtures").to_s
+  register_seedfu_fixtures(Rails.root.join("plugins", "discourse_dislike_webhook", "db", "fixtures").to_s)
 
-    require_dependency 'post_actions_controller'
-    class ::PostActionsController
-        def destroy
-            result = PostActionDestroyer.new(
-              current_user,
-              Post.find_by(id: params[:id].to_i),
-              @post_action_type_id
-            ).perform
+  require_dependency 'post_actions_controller'
+  class ::PostActionsController
+    def destroy
+      result = PostActionDestroyer.new(
+        current_user,
+        Post.find_by(id: params[:id].to_i),
+        @post_action_type_id
+      ).perform
 
-            WebHook.enqueue_remove_like(result.post)
-            
-            if result.failed?
-              render_json_error(result)
-            else
-              render_post_json(result.post, add_raw: false)
-            end
-        end
+      WebHook.enqueue_remove_like(result.post)
+      if result.failed?
+        render_json_error(result)
+      else
+        render_post_json(result.post, add_raw: false)
+      end
     end
+  end
 
-    class ::WebHook
-        def self.enqueue_remove_like(post)
-            payload ||= WebHook.generate_payload(:post, post)
-            if active_web_hooks('post_like_removed').exists?
-                WebHook.enqueue_hooks(
-                    :post_like_removed,
-                    :post_like_removed,
-                    id: post.id,
-                    payload: {
-                        post: post,
-                        user: {
-                            id: post.user.id,
-                            username: post.user.username,
-                        },
-                    }.to_json,
-                )
-            end
-        end
+  class ::WebHook
+    def self.enqueue_remove_like(post)
+      payload ||= WebHook.generate_payload(:post, post)
+      if active_web_hooks('post_like_removed').exists?
+        WebHook.enqueue_hooks(
+          :post_like_removed,
+          :post_like_removed,
+          id: post.id,
+          payload: {
+            post: post,
+            user: {
+              id: post.user.id,
+              username: post.user.username,
+            },
+          }.to_json,
+        )
+      end
     end
+  end
 end
